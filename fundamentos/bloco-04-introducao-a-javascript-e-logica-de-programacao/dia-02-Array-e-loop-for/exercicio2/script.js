8571ede539b84c064b7b2579d7f72fbc77718439
Cristiano Vieira <cristianoviieira@gmail.com>
2022-02-03 18:38:39 -0300

    segundo exercicio do dia

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-Array-e-loop-for/exercicio2/script.js
@@ -0,0 +1,9 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+
+for (let index = 0; index < groceryList.length; index += 1) {
+    // let element = groceryList[index];
+    // console.log(groceryList[index]);
+    let elemento = groceryList[index]
+    console.log(elemento);
+}
