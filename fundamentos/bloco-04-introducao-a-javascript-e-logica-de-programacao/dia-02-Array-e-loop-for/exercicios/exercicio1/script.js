dd93122278589cee4a8a0e08c87a4571ad5e1f0b
Cristiano Vieira <cristianoviieira@gmail.com>
2022-02-04 00:44:40 -0300

    exercicio do dia

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-Array-e-loop-for/exercicios/exercicio1/script.js
@@ -0,0 +1,24 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Exercicio 1
+// console.log(numbers[0]);
+// console.log(numbers[1]);
+// console.log(numbers[2]);
+// console.log(numbers[3]);
+// console.log(numbers[4]);
+// console.log(numbers[5]);
+// console.log(numbers[6]);
+// console.log(numbers[7]);
+// console.log(numbers[8]);
+// console.log(numbers[9]);
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     let conta=numbers[index];
+//     console.log(conta); 
+// }
+
+for (let index = 0; index < numbers.length; index += 1) {
+    let somando = 0;
+    somando += numbers[index];
+    console.log(somando); 
+}
