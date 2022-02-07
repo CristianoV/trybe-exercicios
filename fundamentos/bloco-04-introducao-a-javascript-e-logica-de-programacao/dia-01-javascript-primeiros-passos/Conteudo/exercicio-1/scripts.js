cb08d4966a1fd5552578938498eec059d9a098a1
Cristiano Vieira <cristianoviieira@gmail.com>
2022-02-07 13:29:33 -0300

    exercicios

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/Conteudo/exercicio-1/scripts.js
@@ -0,0 +1,10 @@
+const myName="Cristiano Vieira";
+const birthCity="Novo Hamburgo";
+let birthYear=1996;
+
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
+
+birthYear = 2030;
+console.log(birthYear)
