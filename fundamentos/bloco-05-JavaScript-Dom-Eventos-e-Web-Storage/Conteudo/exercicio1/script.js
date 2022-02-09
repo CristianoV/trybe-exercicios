96fde9a365735c21449577f2682854275c6eb475
Cristiano Vieira <cristianoviieira@gmail.com>
2022-02-09 16:04:58 -0300

    novo exercicio

---
+++ fundamentos/bloco-05-JavaScript-Dom-Eventos-e-Web-Storage/Conteudo/exercicio1/script.js
@@ -0,0 +1,5 @@
+document.getElementById("page-title").innerText = "Xablau";
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+document.getElementById("second-paragraph").innerText = "Testando";
+document.getElementById("subtitle").innerText = "Subtitulo de verdade";
