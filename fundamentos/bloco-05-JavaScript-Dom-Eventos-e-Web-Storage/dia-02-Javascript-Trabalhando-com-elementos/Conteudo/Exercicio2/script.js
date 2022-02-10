5aabf2cd84bf74d812cbfc4a568c2c9b5e0a7ef3
Cristiano Vieira <cristianoviieira@gmail.com>
2022-02-10 16:20:31 -0300

    exercicio 2

---
+++ fundamentos/bloco-05-JavaScript-Dom-Eventos-e-Web-Storage/dia-02-Javascript-Trabalhando-com-elementos/Conteudo/Exercicio2/script.js
@@ -0,0 +1,4 @@
+document.getElementById("elementoOndeVoceEsta").parentElement.style.color="red";
+document.getElementById("primeiroFilhoDoFilho").innerText="Teste";
+let teste = document.getElementById("pai").firstElementChild;
+let teste1 = document.getElementById("elementoOndeVoceEsta").previousElementSibling;
