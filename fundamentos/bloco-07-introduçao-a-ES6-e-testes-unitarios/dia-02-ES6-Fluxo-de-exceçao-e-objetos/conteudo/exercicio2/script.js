3d219618e4de01346d4fa906824cd5b5f835c6e9
Cristiano Vieira <cristianoviieira@gmail.com>
2022-03-07 15:43:28 -0300

    terminando exercicios do dia

---
+++ fundamentos/bloco-07-introduçao-a-ES6-e-testes-unitarios/dia-02-ES6-Fluxo-de-exceçao-e-objetos/conteudo/exercicio2/script.js
@@ -0,0 +1,54 @@
+// const student1 = {
+//     Html: 'Muito Bom',
+//     Css: 'Bom',
+//     JavaScript: 'Ótimo',
+//     SoftSkills: 'Ótimo',
+//   };
+  
+//   const student2 = {
+//     Html: 'Bom',
+//     Css: 'Ótimo',
+//     JavaScript: 'Ruim',
+//     SoftSkills: 'Ótimo',
+//     Git: 'Bom', // chave adicionada
+//   };
+  
+//   let tags = Object.keys(student1);
+
+//     // console.log(Object.keys(student1));
+//     // console.log(Object.keys(tags));
+
+//     for (let index = 0; index < tags.length; index++) {
+//         let nome = tags[index]
+//         let segundo = student1[nome]
+//         console.log(`${nome} ${segundo}`);
+        
+//     }
+
+const student1 = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkills: 'Ótimo',
+  };
+  
+  const student2 = {
+    Html: 'Bom',
+    Css: 'Ótimo',
+    JavaScript: 'Ruim',
+    SoftSkills: 'Ótimo',
+    Git: 'Bom', // chave adicionada
+  };
+  
+  const listSkills = (student) => {
+    const arrayOfSkills = Object.keys(student);
+    for (const index in arrayOfSkills) {
+      console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+    }
+  };
+  
+  console.log('Estudante 1');
+  listSkills(student1);
+  
+  console.log('Estudante 2');
+  listSkills(student2);
